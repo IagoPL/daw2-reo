77a934c83689bdbd4231cd947186e50b051ca2b4
XavierC14 <74736448+XavierC14@users.noreply.github.com>
2022-09-13 22:29:51 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,17 @@
+// Para importar el escaner.
 import java.util.Scanner;
 
 public class Main {
+    // Para permitir poder utilizar el escaner
     static Scanner teclado = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-//hola
+        // Mensaje que saldra al inicio del programa
         System.out.print("Hola, dime tu nombre");
+        // Atributo que sera el nombre que hemos escrito por pantalla
         String nombre = teclado.nextLine();
+        // Parte donde se hacen los pasos para mostrar un mensaje predeterminado segun el nombre escrito
         if (nombre.equals("Iago")) {
             System.out.println("Lo siento, no aceptamos amantes de linux aquí");
         } else if (nombre.equals("Bryan")){
